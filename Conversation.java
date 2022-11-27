cc7c8ffc3e57e99ff43ae23465ff195eb879771c
cat-y <33986615+camtheyam2005@users.noreply.github.com>
2022-11-27 06:33:18 +0000

    added parts 4-6, reading whole address and different foods

--- Conversation.java
+++ Conversation.java
@@ -11,13 +11,22 @@ public class Conversation {
 	int birthyear;
 	double priceOrange;
 	double pricePeanuts;
+  double priceBread;
+  double priceWatermelon;
 	double totalPrice=0;  //initialize to 0
+  int houseNumber;
+  String streetName;
+  String state;
+  String zipcode;
+  String city;
    
     System.out.println("Welcome. Type your first name and press enter.");
 	firstName = reader.nextLine();
+    
 	System.out.println("Type your middle initial and press enter.");
 	input = reader.nextLine();
 	middleInitial = input.charAt(0);
+    
 	System.out.println("Type your last name and press enter.");
 	lastName = reader.nextLine();
    
@@ -30,10 +39,30 @@ public class Conversation {
    
 	//Part 5 code goes here.
 
-   
+
+    System.out.print("Enter house number> ");
+  	houseNumber=reader.nextInt();
+    
+    System.out.print("Enter state> ");
+    reader.nextLine();
+    state = reader.nextLine();
+    
+  
+    System.out.print("Enter street name> ");
+    streetName = reader.nextLine();
+
+    
+    System.out.print("Enter city> ");
+    city = reader.nextLine();
+  
+    System.out.print("Enter zipcode> ");
+    input = reader.nextLine();
+    zipcode = input;
+    
 
     
     //Part 6 Add code to read the rest of the address (city, state, zipcode) and print it
+    System.out.println("Your address is " + houseNumber + " " + streetName + ", " + city + ", " + state + " " + zipcode);
 
 
 
@@ -46,7 +75,12 @@ public class Conversation {
 	pricePeanuts = reader.nextDouble();
 	totalPrice += pricePeanuts;
     //Part 4 code for bread and watermelon
-
+	System.out.print("How much does the bread cost? $");
+	priceBread = reader.nextDouble();
+	totalPrice += priceBread;
+	System.out.print("How much do the watermelons cost? $");
+	priceWatermelon = reader.nextDouble();
+	totalPrice += priceWatermelon;
 
 
 	System.out.println("The total cost is $" + totalPrice);
